80bf923ccb13c958418a0f15b81ef58c4941a031
eemreyyalcin@gmail.com <eemreyyalcin@gmail.com>
2020-05-20 00:16:32 +0300

    added Based Firebase Function

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>, document.querySelector('#root')
+);
